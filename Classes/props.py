e7603f8346d333cf1f55877d1847831bba156d3a
MaitreRenard18 <maitre.renardowo@gmail.com>
2023-02-25 18:44:22 +0100

    Bonhomme de neige + Arbres + Ratio

--- Classes/props.py
+++ Classes/props.py
@@ -1,6 +1,7 @@
 import pygame
 from Classes.tile import Tile
 
+
 def generate_cactus(map, x, y, height):
     if height == 0:
         map.tiles[x][y] = Tile("cactus_top", "cactus_top", minable=False, can_collide=False)
@@ -9,8 +10,40 @@ def generate_cactus(map, x, y, height):
     map.tiles[x][y] = Tile("cactus", "cactus", minable=False, can_collide=False)
     generate_cactus(map, x, y-1, height-1)
 
-def generate_tree(map, x, y, height):
-    pass
+
+def generate_snowman(map, x, y):
+    map.set_tile(x, y, Tile("snowman_belly", "snowman_belly", minable=False, can_collide=False))
+    map.set_tile(x, y-1, Tile("snowman_torso", "snowman_torso", minable=False, can_collide=False))
+    if map.get_tile(x-1, y-1).type == "air":
+        map.set_tile(x-1, y-1, Tile("snowman_right_arm", "snowman_right_arm", minable=False, can_collide=False))
+    if map.get_tile(x+1, y-1).type == "air":
+        map.set_tile(x+1, y-1, Tile("snowman_left_arm", "snowman_left_arm", minable=False, can_collide=False))
+    map.set_tile(x, y-2, Tile("snowman_head", "snowman_head", minable=False, can_collide=False))
+
+
+def generate_tree(map, x, y):
+    def _place_leave(leave_type, x, y):
+        if map.get_tile(x, y).type == "air":
+            map.set_tile(x, y, Tile(leave_type, leave_type, minable=False, can_collide=False))
+    
+    map.set_tile(x, y, Tile("oak_trunk", "oak_trunk", minable=False, can_collide=False))
+    map.set_tile(x, y-1, Tile("oak_trunk", "oak_trunk", minable=False, can_collide=False))
+    map.set_tile(x, y-2, Tile("oak_leaves_covered_trunk", "oak_leaves_covered_trunk", minable=False, can_collide=False))
+    map.set_tile(x, y-3, Tile("oak_dark_leaves", "oak_dark_leaves", minable=False, can_collide=False))
+    
+    for i in range(y-3, y-1):
+        _place_leave("oak_dark_leaves", x-1, i)
+    for i in range(y-3, y-1):
+        _place_leave("oak_dark_leaves", x+1, i)
+
+    for i in range(y-3, y-1):
+        _place_leave("oak_leaves", x-2, i)
+    for i in range(y-3, y-1):
+        _place_leave("oak_leaves", x+2, i)
+
+    for i in range(x-1, x+2):
+        _place_leave("oak_leaves", i, y-4)
+
 
 def generate_snowy_tree(map, x, y, height):
     pass
