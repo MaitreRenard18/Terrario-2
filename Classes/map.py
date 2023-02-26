59e28e10ce5dcacba6c36b126f78145ecf7acfe2
MaitreRenard18 <maitre.renardowo@gmail.com>
2023-02-26 15:28:16 +0100

    Map

--- Classes/map.py
+++ Classes/map.py
@@ -1,4 +1,5 @@
-import pygame, opensimplex, os, random
+import pygame, opensimplex, random
+from pygame import Vector2
 from Classes.player import Player
 from Classes.tile import Tile, Cave
 from Classes.props import *
@@ -44,116 +45,48 @@ ores = {
 }
 
 class Map:
-    def __init__(self):
-        self.display_surface = pygame.display.get_surface()
+    def __init__(self) -> None:
+        self.display_surface: pygame.Surface = pygame.display.get_surface()
 
-        self.tiles = {}
-        self.player = Player((0, 0), self)
+        self._tiles: dict = {}
+        self.player: Player = Player((0, 0), self)
 
-        self.scale = 0.1
+        self.scale: float = 0.1
 
-        self.offset = pygame.Vector2()
-        self.render_distance = 32
+        self.render_distance: int = 32
 
         opensimplex.random_seed()
 
-    def get_tile(self, x: int, y: int) -> Tile:
-        if not x in self.tiles or not y in self.tiles[x]:
-            self.generate_tile(x, y)
+    def get_tile(self, position: Vector2) -> Tile:
+        if not position.x in self.tiles or not position.y in self.tiles[position.x]:
+            self.generate_tile(position)
 
-        return self.tiles[x][y]
+        return self._tiles[position.x][position.y]
 
-    def set_tile(self, tile: Tile, x:int , y: int) -> Tile:
-        if not x in self.tiles or not y in self.tiles[x]:
-            self.generate_tile(x, y)
+    def set_tile(self, tile: Tile, position: Vector2) -> Tile:
+        if not position.x in self.tiles or not position.y in self.tiles[position.x]:
+            self.generate_tile(position)
         
-        self.tiles[x][y] = tile
+        self._tiles[position.x][position.y] = tile
         return Tile
 
-    def generate_tile(self, x, y):
-        if not x in self.tiles:
-            self.tiles[x] = {}
-
-        if y in self.tiles[x]:
-            return
-
-        # Génération de la surface
-        if y < 4:
-            biome = "plains" if opensimplex.noise2(x * 0.0075, y * 0.0075) > 0 else "desert"
-            tile_palette = {
-                "surface_block": "grass" if biome == "plains" else "sand",
-                "primary_block": "dirt" if biome == "plains" else "sand"
-            }
-
-            if y < 0:
-                value = int(opensimplex.noise2(x * self.scale * 0.5, 0) * 10)
-
-                self.tiles[x][y] = Tile("air", "air", minable=False, can_collide=False)
-                if y == value:
-                    self.tiles[x][y] = Tile(tile_palette["surface_block"], tile_palette["surface_block"])
-
-                    if biome == "desert":
-                        if random.randint(0, 10) == 0:
-                            self.generate_tile(x, y-1)
-                            generate_cactus(self, x, y - 1, random.randint(1, 4))
-
-                    else:
-                        if random.randint(0, 2) == 0:
-                            self.generate_tile(x, y-1)
-                            self.tiles[x][y-1].type = "tulip" if random.randint(0, 1) else "weed"
-
-                        if random.randint(0, 10) == 0:
-                            generate_tree(self, x, y-1)
-
-                elif y > value: 
-                    self.tiles[x][y] = Tile(tile_palette["primary_block"], tile_palette["primary_block"])
-
-            elif y == 0:
-                self.generate_tile(x, y-1)
-                type = tile_palette["surface_block"] if self.tiles[x][y-1].type == "air" else tile_palette["primary_block"]
-                self.tiles[x][y] = Tile(type, type)
-
-                if self.tiles[x][y-1].type == "air":
-                    if biome == "desert":
-                        if random.randint(0, 10) == 0:
-                            generate_snowman(self, x, y - 1)
-                    else:
-                        if random.randint(0, 2) == 0:
-                            type = "tulip" if random.randint(0, 1) else "weed"
-                            self.tiles[x][y-1] = Tile(type, type, minable=False, can_collide=False)
-                            
-            else:
-                self.tiles[x][y] = Tile(tile_palette["primary_block"], tile_palette["primary_block"])
-
-        # Génération des grottes
-        else:
-            biome = "plains" if opensimplex.noise2(x * 0.0075, y * 0.0025) > 0 else "desert"
-            tile_palette = {
-                "primary_block": "stone" if biome == "plains" else "sandstone",
-                "cave_block": "cave" if biome == "plains" else "sandstone_cave"
-            }
-
-            if opensimplex.noise2(x * self.scale, y * self.scale) < 0:
-                self.tiles[x][y] = Tile(tile_palette["primary_block"], tile_palette["primary_block"])
-            else:
-                self.tiles[x][y] = Cave("cave", tile_palette["primary_block"])
-            
-    def render(self):
+    def render(self) -> None:
         self.display_surface.fill(pygame.Color(77, 165, 217))
 
-        self.offset.x = self.player.rect.centerx - self.display_surface.get_width() / 2
-        self.offset.y = self.player.rect.centery - self.display_surface.get_height() / 2
+        offset = Vector2()
+        offset.x = self.player.rect.centerx - self.display_surface.get_width() / 2
+        offset.y = self.player.rect.centery - self.display_surface.get_height() / 2
 
         for x in range(int(self.player.position.x) - self.render_distance, int(self.player.position.x) + self.render_distance + 1):
             for y in range(int(self.player.position.y) - self.render_distance, int(self.player.position.y) + self.render_distance + 1):
                 if not x in self.tiles or not y in self.tiles[x]:
                     self.generate_tile(x, y)
 
-                offset_rect = pygame.Vector2(x, y) * 32 - self.offset
+                offset_rect = pygame.Vector2(x, y) * 32 - offset
                 self.tiles[x][y].update(offset_rect)
 
         offset_rect = self.player.rect.copy()
-        offset_rect.center -= self.offset
+        offset_rect.center -= offset
         self.display_surface.blit(self.player.image, offset_rect)
         
         if self.player.going_up == True:
