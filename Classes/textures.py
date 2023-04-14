d846fdb5968e32b6141bece6c6eb33f91051f32b
Wagister <61418245+Wagister@users.noreply.github.com>
2023-04-14 18:41:54 +0200

    Update textures.py

--- Classes/textures.py
+++ Classes/textures.py
@@ -4,14 +4,17 @@ from typing import Dict
 import pygame
 from pygame import Surface, transform
 
+from pathlib import Path
+MODULE_PATH = Path(__file__).parent.parent
+
 def import_textures(path: str, scale: tuple):
     textures: Dict[str, Surface] = {}
-    _textures_path = os.path.join("Images", path)
+    _textures_path = MODULE_PATH / "Images" / path
     for file in os.listdir(_textures_path):
         if file.endswith(".png"):
             file_name = file.replace(".png", "").lower()
 
             path = os.path.join(_textures_path, file)
-            image = transform.scale(pygame.image.load(path), scale)
+            image = pygame.transform.scale(pygame.image.load(path), scale)
             textures[file_name] = image
     return textures
