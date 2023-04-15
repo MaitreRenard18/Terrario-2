6709d3213185ebc39264675999435757f142f1ca
MaitreRenard18 <maitre.renardowo@gmail.com>
2023-04-15 18:32:15 +0200

    Props gravity

--- Classes/prop.py
+++ Classes/prop.py
@@ -22,35 +22,48 @@ for file in os.listdir(_props_path):
 
 
 class Prop:
-    large_hit_box: bool = False
-
     def __init__(self, map: "Map", position: Vector2, prop_name: str) -> None:
         self.map = map
-        self.position: Vector2 = position
         self.prop_name: str = prop_name
         self.tiles: Dict[int, Dict[int, Tile]] = _get_prop(prop_name)
 
+        self.position: Vector2 = Vector2(position)
+        self.tile_pos: Vector2 = Vector2(position)
+
+        self.speed: float = 0.2
+        self.falling: bool = False
+
     def update(self, position: Vector2) -> None:
         display = pygame.display.get_surface()
-        can_fall = True
 
         for x, row in self.tiles.items():
             for y, tile in row.items():
                 display.blit(tile.texture, position + Vector2(x, -y) * 32)
 
-                tile_underneath = self.map.get_tile(self.position + Vector2(x, -y + 1))
-                if self.large_hit_box and tile_underneath.can_collide and not isinstance(tile_underneath, Scaffolding):
-                    can_fall = False
+        self.fall()
 
-        if not self.large_hit_box:
-            tile_underneath = self.map.get_tile(self.position)
-            if tile_underneath.can_collide and not isinstance(tile_underneath, Scaffolding):
-                can_fall = False
+    def fall(self) -> None:
+        tile_below = self.map.get_tile(self.tile_pos)
+        if not tile_below.can_collide and not isinstance(tile_below, Scaffolding):
+            if not self.falling:
+                self.falling = True
+                self.speed = 0
+                return
 
-        if can_fall:
-            self.map.props[self.position.x].pop(self.position.y)
-            self.position = self.position + Vector2(0, 1)
-            self.map.props[self.position.x][self.position.y] = self
+            self.speed += 0.1
+            self.speed = round(self.speed, 1)
+            self.position.y += self.speed
+            self.position.y = round(self.position.y, 1)
+
+            if self.position.y >= self.tile_pos.y + 1:
+                self.tile_pos.y += 1
+                self.position.y = self.tile_pos.y
+                self.map.props[self.position.x].pop(self.position.y - 1)
+                self.map.props[self.position.x][self.position.y] = self
+
+        else:
+            self.falling = False
+            self.speed = 0.2
 
 
 def _get_prop(prop_name: str) -> Dict[int, Dict[int, Tile]]:
