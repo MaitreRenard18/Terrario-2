5337ace0c336a2bc38a0ba380813e962ead63f66
Wagister <61418245+Wagister@users.noreply.github.com>
2023-04-20 01:43:10 +0200

    Update textures.py

--- Classes/textures.py
+++ Classes/textures.py
@@ -7,7 +7,7 @@ from pygame import Surface, transform
 from pathlib import Path
 MODULE_PATH = Path(__file__).parent.parent
 
-def import_textures(path: str, scale: tuple):
+def load_textures(path: str, scale: tuple):
     textures: Dict[str, Surface] = {}
     _textures_path = MODULE_PATH / "Images" / path
     if path.endswith(".png"):
