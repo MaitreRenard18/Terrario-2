146fcf033cdd72e2556d15f8b91b121b9f0d81c0
Wagister <61418245+Wagister@users.noreply.github.com>
2023-04-22 22:02:29 +0200

    Update textures.py

--- Classes/textures.py
+++ Classes/textures.py
@@ -37,3 +37,30 @@ def load_animated_textures(path: str, size: Union[Vector2, tuple]):
                                                image.get_height() * (size[0] // image.get_width())))
 
         return [image.subsurface((0, i * y, 32, 32)) for i in range(image.get_height() // y)]
+    
+    
+# Charge les textures concernant le menu et les différents boutons.
+logo_texture: Surface = load_textures("UI/logo.png", (970, 116))
+background_textures: Dict[str, Surface] = load_textures("Screenshots", (1920, 1080))
+thumbnails_textures: Dict[str, Surface] = load_textures("Thumbnails", (125, 125))
+button_textures: Dict[str, Surface] = {"p_button": load_textures("Button/p_button.png", (360, 108)),
+                                       "up_button": load_textures("Button/up_button.png", (360, 108)),
+                                       "button_hovered": load_textures("Button/button_hovered.png", (360, 108)),}
+world_textures: Dict[str, Surface] = {"world_button": load_textures("Button/world_button.png", (450, 135)),
+                                      "world_button_hovered": load_textures("Button/world_button_hovered.png", (450, 135))}
+delete_textures: Dict[str, Surface] = {"delete_button": load_textures("Button/delete_button.png", (135, 135)),
+                                      "delete_button_hovered": load_textures("Button/delete_button_hovered.png", (135, 135))}
+x_mark_textures: Dict[str, Surface] = {"x_mark": load_textures("Button/x_mark.png", (56, 56)),
+                                      "x_mark_hovered": load_textures("Button/x_mark_hovered.png", (56, 56))}
+
+# Charge les textures concernant le joueur, les minerais, l'inventaire et l'interface de craft.
+player_textures: Dict[str, Surface] = load_textures("Player", (32, 32))
+ores_textures: Dict[str, Surface] = load_textures("Ores", (96, 96))
+inventory_texture: Surface = load_textures("UI/inventory.png", (942, 462))
+craft_interface_texture: Surface = load_textures("UI/craft_interface.png", (840, 390))
+drilltip_textures: Dict[str, Surface] = {}
+for level in range(1, 9):
+    drilltip_textures["drilltip_right_" + str(level)] = load_textures("Player/drilltip_right_" + str(level) + ".png", (192, 192))
+    
+# Charge les textures concernant les tiles qui composent la map.
+tiles_textures = load_textures("Tiles", (32, 32))
