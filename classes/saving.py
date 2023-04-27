c75dd01e1800f5e7f56fd0f7860c71ad50aadc89
MaitreRenard18 <63685473+MaitreRenard18@users.noreply.github.com>
2023-04-27 13:44:51 +0200

    docstrings

--- classes/saving.py
+++ classes/saving.py
@@ -1,32 +1,66 @@
-from typing import List
+from typing import List, TYPE_CHECKING
 import pickle
 import os
 
 import pygame.image
 
-from .constants import MODULE_PATH
+from .constants import MODULE_PATH, screen
+if TYPE_CHECKING:
+    from classes.map import Map
 
+
+# Créer un dossier "saves" stockant les sauvegardes des mondes
 if "saves" not in os.listdir(MODULE_PATH):
     os.mkdir(MODULE_PATH / "saves")
-
+    
+# Créer un dossier "thumbnails" stockant les miniatures des mondes
 if "thumbnails" not in os.listdir(MODULE_PATH / "images"):
     os.mkdir(MODULE_PATH / "images/thumbnails")
  
+# Stock le chemin des différents dossiers
 _saves_path: str = MODULE_PATH / "saves"
 _thumbnail_path: str = MODULE_PATH / "images" / "thumbnails"
 
+    
+def get_thumbnail() -> Surface:
+    """
+    Retourne une surface au format 1:1 de ce qui est actuellement affiché sur l'écran.
+    """
 
+    if screen.get_width() > screen.get_height():
+        return screen.subsurface((
+            screen.get_width() // 2 - screen.get_height() // 2, 0,
+            screen.get_height(), screen.get_height()))
+
+    else:
+        return screen.subsurface((
+            0, screen.get_height() // 2 - screen.get_width() // 2,
+            screen.get_width(), screen.get_width()))
+
+    
 def get_saves() -> List[str]:
+    """
+    Retourne une liste contenant le nom de toutes les sauvegardes.
+    """
+    
     return [file for file in os.listdir(_saves_path) if not file.endswith(".png")]
 
-from .map import Map
-def save(file_name, map: Map) -> None:
+
+def save(file_name, map: "Map") -> None:
+    """
+    Sauvegarde la Map passée en paramètre dans le fichier du nom également passé en paramètre.
+    """
+    
     with open(_saves_path / file_name, "wb") as file:
-        thumbnail = map.get_thumbnail()
+        thumbnail = get_thumbnail()
         pygame.image.save_extended(thumbnail, _thumbnail_path / f"{file_name}.png")
         pickle.dump(map, file)
 
 
-def load(file_name: str) -> Map:
+def load(file_name: str) -> "Map":
+    """
+    Charge le fichier passé en paramètre.
+    """
+    
     with open(_saves_path / file_name, "rb") as file:
         return pickle.load(file)
